45915ea0232409d9c82a928fa8010991e2d87976
Mohammad Abdullah <72933566+8808533052@users.noreply.github.com>
2021-08-17 15:23:42 +0530

    Day1 Task

---
+++ src/components/ComponentA.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComponentA = () => {
+    return (
+        <>
+           <h1>This is a Component A</h1> 
+        </>
+    )
+}
+
+export default ComponentA
---
+++ src/components/ComponentB.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComponentB = () => {
+    return (
+        <>
+            <h1>This is a Component B</h1>
+        </>
+    )
+}
+
+export default ComponentB
